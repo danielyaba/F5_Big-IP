ccbb8faf5abfd5fb5c72730bebf4e43bebd6bbea
danielyaba <97508628+danielyaba@users.noreply.github.com>
2022-01-17 14:19:49 +0200

    Create irule_SSL_DECRYPT.tcl

---
+++ irule_SSL_DECRYPT.tcl
@@ -0,0 +1,17 @@
+#!iRule
+# Decrypt TLS Traffic
+
+# grep Session-ID /var/log/ltm | sed 's/.*\(RSA.*\)/\1/' > /var/tmp/SSLDUMP.pmsk
+
+when CLIENTSSL_HANDSHAKE {
+    if { [IP::addr [IP::client_addr] equals 1.1.1.1] } {
+	    log local0.info "Client Side: RSA Session-ID:[SSL::sessionid] Master-Key:[SSL::sessionsecret]"
+    }
+}
+
+when SERVERSSL_HANDSHAKE {
+	  if { [IP::addr [IP::client_addr] equals 1.1.1.1] } {
+	     log local0.info "Server Side: RSA Session-ID:[SSL::sessionid] Master-Key:[SSL::sessionsecret]"
+    }
+}
+
